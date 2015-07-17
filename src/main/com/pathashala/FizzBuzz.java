8b3450f63e933c5a2c05462e6474656d7d1a8dea
varshapalat <varshapalat@gmail.com>
2015-07-17 14:50:12 +0530

    final

--- src/main/com/pathashala/FizzBuzz.java
+++ src/main/com/pathashala/FizzBuzz.java
@@ -4,19 +4,15 @@ public class FizzBuzz {
     private static int value;
 
     public FizzBuzz(int i) {
-        value=i;
+        value = i;
     }
 
     public static String multiple() {
-        if(value%15 == 0)
-        {
+        if (value % 15 == 0) {
             return "fizzbuzz";
-        }
-        else if(value%3 == 0) {
+        } else if (value % 3 == 0) {
             return "fizz";
-        }
-        else if(value%5 == 0)
-        {
+        } else if (value % 5 == 0) {
             return "buzz";
         }
         return "blah";
--- src/test/com/pathashala/FizzBuzzTest.java
+++ src/test/com/pathashala/FizzBuzzTest.java
@@ -6,48 +6,52 @@ import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
     @Test
-    public void shouldReturnFizzIfNumberIsThree(){
+    public void shouldReturnFizzIfNumberIsThree() {
         FizzBuzz fb = new FizzBuzz(3);
         String result = FizzBuzz.multiple();
-        assertEquals("fizz",result);
+        assertEquals("fizz", result);
     }
+
     @Test
-    public void shouldReturnBuzzIfNumberIsFive(){
+    public void shouldReturnBuzzIfNumberIsFive() {
         FizzBuzz fb = new FizzBuzz(5);
         String result = FizzBuzz.multiple();
-        assertEquals("buzz",result);
+        assertEquals("buzz", result);
     }
+
     @Test
-    public void shouldReturnFizzBuzzIfNumberIsFifteen(){
+    public void shouldReturnFizzBuzzIfNumberIsFifteen() {
         FizzBuzz fb = new FizzBuzz(15);
         String result = FizzBuzz.multiple();
-        assertEquals("fizzbuzz",result);
+        assertEquals("fizzbuzz", result);
     }
+
     @Test
-    public void shouldReturnBlahIfNumberIsNotAMultipleOfThreeOrFive(){
+    public void shouldReturnBlahIfNumberIsNotAMultipleOfThreeOrFive() {
         FizzBuzz fb = new FizzBuzz(4);
         String result = FizzBuzz.multiple();
-        assertEquals("blah",result);
+        assertEquals("blah", result);
     }
+
     @Test
-    public void shouldReturnFizzIfNumberIsMultipleOfThree(){
+    public void shouldReturnFizzIfNumberIsMultipleOfThree() {
         FizzBuzz fb = new FizzBuzz(9);
         String result = FizzBuzz.multiple();
-        assertEquals("fizz",result);
+        assertEquals("fizz", result);
     }
 
     @Test
-    public void shouldReturnBuzzIfNumberIsMultipleOfFive(){
+    public void shouldReturnBuzzIfNumberIsMultipleOfFive() {
         FizzBuzz fb = new FizzBuzz(20);
         String result = FizzBuzz.multiple();
-        assertEquals("buzz",result);
+        assertEquals("buzz", result);
     }
 
     @Test
-    public void shouldReturnBuzzIfNumberIsMultipleOfFifteen(){
+    public void shouldReturnBuzzIfNumberIsMultipleOfFifteen() {
         FizzBuzz fb = new FizzBuzz(60);
         String result = FizzBuzz.multiple();
-        assertEquals("fizzbuzz",result);
+        assertEquals("fizzbuzz", result);
     }
 
 
